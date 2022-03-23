dfb68fe068ee47029c0bc8eed8f2f709ad07b9ae
Gia Huy <huynhg@mapmaker.asia>
2022-03-23 10:39:29 +0700

    Add project files.

---
+++ App5/App5/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App5
+{
+    public partial class MainPage : ContentPage
+    {
+        int i = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Counter.Text = ""+ i++;
+        }
+    }
+}
